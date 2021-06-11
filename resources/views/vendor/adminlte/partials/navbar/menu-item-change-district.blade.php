452d7ad08fd3271358e5b3eb33f57a180bc345e3
Mateusz Hajder <6783135+mhajder@users.noreply.github.com>
2021-06-11 22:49:33 +0200

    Add other AdminLTE views

---
+++ resources/views/vendor/adminlte/partials/navbar/menu-item-change-district.blade.php
@@ -0,0 +1,23 @@
+@php
+    $districts = \App\Models\District::whereNull('parent_id')->get();
+    $userSelectedDistrict = $districts->find(Auth::user()->selected_district);
+@endphp
+@if(sizeof($districts) >= 1)
+<li class="nav-item dropdown ">
+    <a href="#" class="nav-link" data-toggle="dropdown" aria-expanded="true">
+        <i class="fas fa-fw fa-flag"></i>
+        {{ $userSelectedDistrict->name }}
+    </a>
+    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+        <span class="dropdown-item dropdown-header">{{ __('adminlte::menu.change_district') }}</span>
+        @foreach ($districts as $district)
+            @if($district->id != Auth::user()->selected_district)
+                <div class="dropdown-divider"></div>
+                <a href="{{ route('district.change', ['district_id' => $district->id ]) }}" class="dropdown-item">
+                    {{ $district->name }}
+                    <span class="float-right text-muted text-sm">{{ $district->code }}</span>
+                </a>
+            @endif
+        @endforeach
+</li>
+@endif
