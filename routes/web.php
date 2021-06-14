4106e389965cbbe4abbadbd6a4e4121df3b821fb
Mateusz Hajder <6783135+mhajder@users.noreply.github.com>
2021-06-14 12:32:51 +0200

    Change route parameters validation to global

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::patch('/profile', [App\Http\Controllers\ProfileController::class, 'update
 
 // ChangeDistrictController
 Route::get('/district/change/{district_id}', [App\Http\Controllers\ChangeDistrictController::class, 'changeCurrent'])
-    ->where('district_id', '[0-9]+')->name('district.change');
+    ->name('district.change');
 
 // AuthorizationController
 Route::get('/authorizations', [App\Http\Controllers\AuthorizationController::class, 'index'])->name('authorization.index');
@@ -41,37 +41,37 @@ Route::post('/authorization', [App\Http\Controllers\AuthorizationController::cla
 Route::get('/hunting-book', [App\Http\Controllers\HuntingBookController::class, 'index'])->name('hunting.index');
 Route::get('/hunting/create', [App\Http\Controllers\HuntingBookController::class, 'create'])->name('hunting.create');
 Route::post('/hunting', [App\Http\Controllers\HuntingBookController::class, 'store'])->name('hunting.store');
-Route::get('/hunting/edit/{id}', [App\Http\Controllers\HuntingBookController::class, 'edit'])->where('id', '[0-9]+')->name('hunting.edit');
-Route::patch('/hunting/{id}', [App\Http\Controllers\HuntingBookController::class, 'update'])->where('id', '[0-9]+')->name('hunting.update');
+Route::get('/hunting/edit/{id}', [App\Http\Controllers\HuntingBookController::class, 'edit'])->name('hunting.edit');
+Route::patch('/hunting/{id}', [App\Http\Controllers\HuntingBookController::class, 'update'])->name('hunting.update');
 
-Route::patch('/hunting/cancel/{id}', [App\Http\Controllers\HuntingBookController::class, 'cancel'])->where('id', '[0-9]+')->name('hunting.cancel');
-Route::patch('/hunting/finish/{id}', [App\Http\Controllers\HuntingBookController::class, 'finish'])->where('id', '[0-9]+')->name('hunting.finish');
+Route::patch('/hunting/cancel/{id}', [App\Http\Controllers\HuntingBookController::class, 'cancel'])->name('hunting.cancel');
+Route::patch('/hunting/finish/{id}', [App\Http\Controllers\HuntingBookController::class, 'finish'])->name('hunting.finish');
 
 Route::middleware('can:admin-only')->group(function () {
     // UserController
     Route::get('/admin/users', [App\Http\Controllers\Admin\UserController::class, 'index'])->name('user.index');
     Route::get('/admin/user/create', [App\Http\Controllers\Admin\UserController::class, 'create'])->name('user.create');
     Route::post('/admin/user/create', [App\Http\Controllers\Admin\UserController::class, 'store'])->name('user.store');
-    Route::get('/admin/user/edit/{id}', [App\Http\Controllers\Admin\UserController::class, 'edit'])->where('id', '[0-9]+')->name('user.edit');
-    Route::patch('/admin/user/edit/{id}', [App\Http\Controllers\Admin\UserController::class, 'update'])->where('id', '[0-9]+')->name('user.update');
-    Route::patch('/admin/user/block/{id}', [App\Http\Controllers\Admin\UserController::class, 'block'])->where('id', '[0-9]+')->name('user.block');
+    Route::get('/admin/user/edit/{id}', [App\Http\Controllers\Admin\UserController::class, 'edit'])->name('user.edit');
+    Route::patch('/admin/user/edit/{id}', [App\Http\Controllers\Admin\UserController::class, 'update'])->name('user.update');
+    Route::patch('/admin/user/block/{id}', [App\Http\Controllers\Admin\UserController::class, 'block'])->name('user.block');
 
     // DistrictController
     Route::get('/admin/districts', [App\Http\Controllers\Admin\DistrictController::class, 'index'])->name('district.index');
     Route::get('/admin/district/create', [App\Http\Controllers\Admin\DistrictController::class, 'create'])->name('district.create');
     Route::post('/admin/district', [App\Http\Controllers\Admin\DistrictController::class, 'store'])->name('district.store');
-    Route::get('/admin/district/edit/{id}', [App\Http\Controllers\Admin\DistrictController::class, 'edit'])->where('id', '[0-9]+')->name('district.edit');
-    Route::patch('/admin/district/{id}', [App\Http\Controllers\Admin\DistrictController::class, 'update'])->where('id', '[0-9]+')->name('district.update');
+    Route::get('/admin/district/edit/{id}', [App\Http\Controllers\Admin\DistrictController::class, 'edit'])->name('district.edit');
+    Route::patch('/admin/district/{id}', [App\Http\Controllers\Admin\DistrictController::class, 'update'])->name('district.update');
 
     // HuntingGroundController
     Route::get('/admin/district/{district_id}/hunting-grounds', [App\Http\Controllers\Admin\HuntingGroundController::class, 'index'])
-        ->where('district_id', '[0-9]+')->name('hunting-ground.index');
+        ->name('hunting-ground.index');
     Route::get('/admin/district/{district_id}/hunting-ground/create', [App\Http\Controllers\Admin\HuntingGroundController::class, 'create'])
-        ->where('district_id', '[0-9]+')->name('hunting-ground.create');
+        ->name('hunting-ground.create');
     Route::post('/admin/district/{district_id}/hunting-ground', [App\Http\Controllers\Admin\HuntingGroundController::class, 'store'])
-        ->where('district_id', '[0-9]+')->name('hunting-ground.store');
+        ->name('hunting-ground.store');
     Route::get('/admin/district/{district_id}/hunting-ground/edit/{id}', [App\Http\Controllers\Admin\HuntingGroundController::class, 'edit'])
-        ->where('district_id', '[0-9]+')->where('id', '[0-9]+')->name('hunting-ground.edit');
+        ->name('hunting-ground.edit');
     Route::patch('/admin/district/{district_id}/hunting-ground/{id}', [App\Http\Controllers\Admin\HuntingGroundController::class, 'update'])
-        ->where('district_id', '[0-9]+')->where('id', '[0-9]+')->name('hunting-ground.update');
+        ->name('hunting-ground.update');
 });
